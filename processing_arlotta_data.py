3991fea2cf6d6cf1608702aef11cbc9a4cd2a61c
saulvegasauceda <86434516+saulvegasauceda@users.noreply.github.com>
2023-01-30 17:58:34 -0800

    fixing up processing steps

--- processing_arlotta_data.py
+++ processing_arlotta_data.py
@@ -2,10 +2,6 @@ import anndata
 import scvelo as scv
 import scanpy as sc
 import pandas as pd
-import numpy as np
-import seaborn as sbn
-import scipy
-import matplotlib.pyplot as plt
 
 path_to_umap = "/home/gridsan/ssauceda/neuroTF_shared/mouse_cortex_development/umap/"
 umap_coords_file = path_to_umap + "cluster_scDevSC.merged.umap.txt"
@@ -42,24 +38,24 @@ adata = adata[barcodes_excitatory]
 
 sum_spliced_features = np.array(adata.layers["spliced"].sum(axis=1))
 sum_unspliced_features = np.array(adata.layers["unspliced"].sum(axis=1))
-
 # filter out cells with less than 500 spliced or unspliced features
 cells_w_enough_splice_info = np.asarray((sum_spliced_features + sum_unspliced_features) >= 500)
-
 adata = adata[cells_w_enough_splice_info, :]
 
+# Traditional pre-processing steps
 scv.pp.normalize_per_cell(adata, counts_per_cell_after=1e4, enforce=True)
 sc.pp.highly_variable_genes(adata, flavor='seurat_v3', n_top_genes=3000)
 scv.pp.log1p(adata)
-
 regress_out = ['nCount_RNA', 'nFeature_RNA', 'percent_mito', 'CC_Difference']
 sc.pp.regress_out(adata, keys=regress_out)
 
+# scVelo setup
 scv.pp.moments(adata, n_pcs=30, n_neighbors=30, use_highly_variable=True)
 scv.tl.recover_dynamics(adata, n_jobs=20)
 scv.tl.velocity(adata, mode='dynamical', use_highly_variable=True)
 scv.tl.velocity_graph(adata, n_jobs=20)
 
+# Run trajectory algorithms
 scv.tl.latent_time(adata)
 scv.tl.velocity_pseudotime(adata)
 # this is needed due to a current bug
