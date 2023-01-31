55cdf8301048f8497d03b60fa3bde22abd9e1bac
saulvegasauceda <86434516+saulvegasauceda@users.noreply.github.com>
2023-01-30 16:48:11 -0800

    adding regress lines

--- processing_arlotta_data.py
+++ processing_arlotta_data.py
@@ -52,6 +52,9 @@ scv.pp.normalize_per_cell(adata, counts_per_cell_after=1e4, enforce=True)
 sc.pp.highly_variable_genes(adata, flavor='seurat_v3', n_top_genes=3000)
 scv.pp.log1p(adata)
 
+regress_out = ['nCount_RNA', 'nFeature_RNA', 'percent_mito', 'CC_Difference']
+sc.pp.regress_out(adata, keys=regress_out)
+
 scv.pp.moments(adata, n_pcs=30, n_neighbors=30, use_highly_variable=True)
 scv.tl.recover_dynamics(adata, n_jobs=20)
 scv.tl.velocity(adata, mode='dynamical', use_highly_variable=True)
